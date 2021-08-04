056cdfe204e0625ec3c9bbe966d2176ae265bc66
nero3327 <43396258+nero3327@users.noreply.github.com>
2021-08-04 08:51:06 +0800

    Create loop2.bat

---
+++ loop2.bat
@@ -0,0 +1,36 @@
+:begin
+set Index=0
+rem pause
+
+:restart
+
+
+
+
+
+
+
+youtube-dl --cookies "your_cookies_file" https://www.youtube.com/playlist?list=WL
+
+
+
+
+
+
+set /a Index = Index+1
+echo %Index%
+if %Index%==20 goto juj
+goto restart
+
+
+:juj
+rem pause
+set /p SE=请选择aGain或者eXit
+
+if %SE%==a goto begin
+if %SE%==e goto exit
+
+
+:exit
+rem pause
+exit
